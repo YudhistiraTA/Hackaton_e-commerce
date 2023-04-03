9eeea29d3b37339bbb8aa401b07c9923ce6e9256
Yudhistira Tribuana Authar <yudhistira.tribuana@gmail.com>
2023-04-03 09:12:54 +0700

    Landing page functionality

--- items.js
+++ items.js
@@ -1,28 +1,83 @@
 const basketball = [
     {
-      name: "Basketball Ball",
-      price: 215400,
-      img_src: "assets/basket_bola.jpg",
+        name: "Basketball Ball",
+        price: 215400,
+        img_src: "assets/basket_bola.jpg",
     },
     {
-      name: "Basketball Shoes",
-      price: 1383200,
-      img_src: "assets/basket_sepatu.jpg",
+        name: "Basketball Shoes",
+        price: 1383200,
+        img_src: "assets/basket_sepatu.jpg",
     },
     {
-      name: "Basketball Shorts",
-      price: 80000,
-      img_src: "assets/basket_celana.png",
+        name: "Basketball Shorts",
+        price: 80000,
+        img_src: "assets/basket_celana.png",
     },
     {
-      name: "Basketball Shirt",
-      price: 34000,
-      img_src: "assets/basket_kaos.jpg",
+        name: "Basketball Shirt",
+        price: 34000,
+        img_src: "assets/basket_kaos.jpg",
     },
     {
-      name: "Basketball Jersey",
-      price: 99000,
-      img_src: "assets/basket_jersey.png",
+        name: "Basketball Jersey",
+        price: 99000,
+        img_src: "assets/basket_jersey.png",
     },
-  ];
-  
+];
+
+const football = [
+    {
+        name: "Football",
+        price: 215400,
+        img_src: "assets/basket_bola.jpg",
+    },
+    {
+        name: "Football Shoes",
+        price: 1383200,
+        img_src: "assets/basket_sepatu.jpg",
+    },
+    {
+        name: "Football Shorts",
+        price: 80000,
+        img_src: "assets/basket_celana.png",
+    },
+    {
+        name: "Football Shirt",
+        price: 34000,
+        img_src: "assets/basket_kaos.jpg",
+    },
+    {
+        name: "Football Jersey",
+        price: 99000,
+        img_src: "assets/basket_jersey.png",
+    },
+];
+
+const badminton = [
+    {
+        name: "Shuttlecock",
+        price: 215400,
+        img_src: "assets/basket_bola.jpg",
+    },
+    {
+        name: "Badminton racket",
+        price: 1383200,
+        img_src: "assets/basket_sepatu.jpg",
+    },
+    {
+        name: "Badminton shoes",
+        price: 80000,
+        img_src: "assets/basket_celana.png",
+    },
+    {
+        name: "Badminton net",
+        price: 34000,
+        img_src: "assets/basket_kaos.jpg",
+    },
+    {
+        name: "Lapangan",
+        price: 99000,
+        img_src: "assets/basket_jersey.png",
+    },
+];
--- scripts.js
+++ scripts.js
@@ -1,43 +1,61 @@
 function createCard(item) {
-  const card = document.createElement("div");
-  card.className = "card";
+    const card = document.createElement("div");
+    card.className = "card";
 
-  const img = document.createElement("img");
-  img.src = item.img_src;
+    const img = document.createElement("img");
+    img.src = item.img_src;
 
-  const cardTitle = document.createElement("div");
-  cardTitle.className = "card-title";
-  cardTitle.textContent = item.name;
+    const cardTitle = document.createElement("div");
+    cardTitle.className = "card-title";
+    cardTitle.textContent = item.name;
 
-  const cardPrice = document.createElement("div");
-  let tempStr = String(item.price);
-  let tempArr = [];
-  for (let i = tempStr.length - 1, dot = 0; i >= 0; i--) {
-    if (dot++ === 3) {
-      tempArr.push('.');
-      dot = 1;
+    const cardPrice = document.createElement("div");
+    let tempStr = String(item.price);
+    let tempArr = [];
+    for (let i = tempStr.length - 1, dot = 0; i >= 0; i--) {
+        if (dot++ === 3) {
+            tempArr.push('.');
+            dot = 1;
+        }
+        tempArr.push(tempStr[i]);
     }
-    tempArr.push(tempStr[i]);
-  }
-  itemPrice = tempArr.reverse().join("");
-  cardPrice.className = "card-price";
-  cardPrice.textContent = `Rp. ${itemPrice}`;
+    itemPrice = tempArr.reverse().join("");
+    cardPrice.className = "card-price";
+    cardPrice.textContent = `Rp. ${itemPrice}`;
 
-  card.appendChild(img);
-  card.appendChild(cardTitle);
-  card.appendChild(cardPrice);
+    card.appendChild(img);
+    card.appendChild(cardTitle);
+    card.appendChild(cardPrice);
 
-  return card;
+    return card;
 }
 
+const itemRow = document.querySelector("#item-row");
+
 function displayItems(items) {
-  const itemRow = document.querySelector("#item-row");
-  items.forEach((item) => {
-    const card = createCard(item);
-    const tableCell = document.createElement("td");
-    tableCell.appendChild(card);
-    itemRow.appendChild(tableCell);
-  });
+    items.forEach((item) => {
+        const card = createCard(item);
+        const tableCell = document.createElement("td");
+        tableCell.appendChild(card);
+        itemRow.appendChild(tableCell);
+    });
 }
 
 displayItems(basketball);
+document.getElementById("basketball-category").addEventListener("click", function () {
+    if (itemRow.hasChildNodes())
+        itemRow.innerHTML = '';
+    displayItems(basketball);
+})
+
+document.getElementById("football-category").addEventListener("click", function () {
+    if (itemRow.hasChildNodes())
+        itemRow.innerHTML = '';
+    displayItems(football);
+})
+
+document.getElementById("badminton-category").addEventListener("click", function () {
+    if (itemRow.hasChildNodes())
+        itemRow.innerHTML = '';
+    displayItems(badminton);
+})

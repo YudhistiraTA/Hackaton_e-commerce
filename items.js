ac766d639b196c6544fb30d32b46e4fb8bcf3d15
Dicky <dickyadityo25@gmail.com>
2023-04-05 22:03:31 +0700

    nambahin deskripsi

--- items.js
+++ items.js
@@ -3,26 +3,31 @@ const basketball = [
         name: "Basketball Ball",
         price: 215400,
         img_src: "assets/basket_bola.jpg",
+        description: `Reprehenderit sequi nulla repudiandae laborum. Et inventore voluptas reiciendis qui. Et ut maxime aperiam.`
     },
     {
         name: "Basketball Shoes",
         price: 1383200,
         img_src: "assets/basket_sepatu.jpg",
+        description: `Reprehenderit sequi nulla repudiandae laborum. Et inventore voluptas reiciendis qui. Et ut maxime aperiam.`
     },
     {
         name: "Basketball Shorts",
         price: 80000,
         img_src: "assets/basket_celana.png",
+        description: `Reprehenderit sequi nulla repudiandae laborum. Et inventore voluptas reiciendis qui. Et ut maxime aperiam.`
     },
     {
         name: "Basketball Shirt",
         price: 34000,
         img_src: "assets/basket_kaos.jpg",
+        description: `Reprehenderit sequi nulla repudiandae laborum. Et inventore voluptas reiciendis qui. Et ut maxime aperiam.`
     },
     {
         name: "Basketball Jersey",
         price: 99000,
         img_src: "assets/basket_jersey.png",
+        description: `Reprehenderit sequi nulla repudiandae laborum. Et inventore voluptas reiciendis qui. Et ut maxime aperiam.`
     },
 ];
 
@@ -31,26 +36,31 @@ const football = [
         name: "Football",
         price: 250000,
         img_src: "assets/soccer-ball.jpeg",
+        description: `Reprehenderit sequi nulla repudiandae laborum. Et inventore voluptas reiciendis qui. Et ut maxime aperiam.`
     },
     {
         name: "Football Shoes",
         price: 1450000,
         img_src: "assets/soccer-shoes.jpg",
+        description: `Reprehenderit sequi nulla repudiandae laborum. Et inventore voluptas reiciendis qui. Et ut maxime aperiam.`
     },
     {
         name: "Football Glove",
         price: 450000,
         img_src: "assets/soccer-glove.jpg",
+        description: `Reprehenderit sequi nulla repudiandae laborum. Et inventore voluptas reiciendis qui. Et ut maxime aperiam.`
     },
     {
         name: "Football Jersey",
         price: 150000,
         img_src: "assets/soccer-jersey.jpg",
+        description: `Reprehenderit sequi nulla repudiandae laborum. Et inventore voluptas reiciendis qui. Et ut maxime aperiam.`
     },
     {
         name: "Football Shin Guard",
         price: 125000,
         img_src: "assets/soccer-shin-guard.jpg",
+        description: `Reprehenderit sequi nulla repudiandae laborum. Et inventore voluptas reiciendis qui. Et ut maxime aperiam.`
     },
 ];
 
@@ -59,25 +69,30 @@ const badminton = [
         name: "Shuttlecock",
         price: 100000,
         img_src: "assets/badminton-shuttlecock.jpg",
+        description: `Reprehenderit sequi nulla repudiandae laborum. Et inventore voluptas reiciendis qui. Et ut maxime aperiam.`
     },
     {
         name: "Badminton racket",
         price: 550000,
         img_src: "assets/badminton-racket.jpg",
+        description: `Reprehenderit sequi nulla repudiandae laborum. Et inventore voluptas reiciendis qui. Et ut maxime aperiam.`
     },
     {
         name: "Badminton shoes",
         price: 980000,
         img_src: "assets/badminton-shoes.jpg",
+        description: `Reprehenderit sequi nulla repudiandae laborum. Et inventore voluptas reiciendis qui. Et ut maxime aperiam.`
     },
     {
         name: "Badminton net",
         price: 790000,
         img_src: "assets/badminton-net.jpg",
+        description: `Reprehenderit sequi nulla repudiandae laborum. Et inventore voluptas reiciendis qui. Et ut maxime aperiam.`
     },
     {
         name: "Grip tape",
         price: 250000,
         img_src: "assets/badminton-griptape.jpg",
+        description: `Reprehenderit sequi nulla repudiandae laborum. Et inventore voluptas reiciendis qui. Et ut maxime aperiam.`
     },
 ];
